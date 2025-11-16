683d80b3bd2f844292f33d400ef4d45fb9ba5835
seungyeonchoi <sychoi@pulip.com>
2025-11-17 08:35:03 +0900

    dist 파일

--- dist/guides/list/ia/data_a.js
+++ dist/guides/list/ia/data_a.js
@@ -2,15 +2,16 @@ const iaPageAData = {
 	language: "ko",
 	list: [
 		// --- 메인 ---
-		{ createDate: "",  modifyDate: "",  category: "메인", depth1: "",  depth2: "",  depth3: "-",  pageName: "메인",  pageType: "page",  pageID: "MA_01",  link: "", isAdminLinked: true,  status: "",  memo: [] },
+		{ createDate: "",  modifyDate: "",  category: "메인", depth1: "",  depth2: "",  depth3: "-",  pageName: "메인",  pageType: "page",  pageID: "MA_01",  link: "", isAdminLinked: true,  status: "작업중",  memo: [] },
 		{ createDate: "", modifyDate: "", category: "메인", depth1: "메인 공지 팝업", depth2: "", depth3: "-", pageName: "메인공지팝업", pageType: "popup", pageID: "MA_02P", link: "", isAdminLinked: false, status: "", memo: [], },
-		{ createDate: "", modifyDate: "", category: "메인", depth1: "검색", depth2: "", depth3: "-", pageName: "검색", pageType: "popup", pageID: "MA_03P", link: "", isAdminLinked: false, status: "", memo: [], },
-		{ createDate: "", modifyDate: "", category: "메인", depth1: "", depth2: "검색 결과", depth3: "-", pageName: "검색결과", pageType: "page", pageID: "MA_03_01", link: "", isAdminLinked: false, status: "", memo: [], },
+		{ createDate: "", modifyDate: "", category: "메인", depth1: "검색", depth2: "", depth3: "-", pageName: "검색", pageType: "popup", pageID: "MA_03P", link: "", isAdminLinked: false, status: "작업중", memo: [], },
+		{ createDate: "", modifyDate: "", category: "메인", depth1: "", depth2: "검색 결과", depth3: "-", pageName: "검색결과", pageType: "page", pageID: "MA_03_01", link: "", isAdminLinked: false, status: "작업중", memo: [], },
+		{ createDate: "", modifyDate: "", category: "메인", depth1: "", depth2: "검색 결과", depth3: "-", pageName: "검색결과 없음", pageType: "page", pageID: "MA_03_02", link: "", isAdminLinked: false, status: "작업중", memo: [], },
 		{ createDate: "", modifyDate: "", category: "메인", depth1: "전체 메뉴", depth2: "", depth3: "-", pageName: "전체메뉴", pageType: "popup", pageID: "MA_04P", link: "", isAdminLinked: false, status: "", memo: [], },
 
 		// --- 한화펀드 ---
-		{ createDate: "", modifyDate: "", category: "한화펀드", depth1: "펀드찾기", depth2: "-", depth3: "-", pageName: "펀드찾기", pageType: "page", pageID: "FD_01", link: "", isAdminLinked: true, status: "", memo: [], },
-		{ createDate: "", modifyDate: "", category: "한화펀드", depth1: "펀드찾기", depth2: "펀드 상품 비교", depth3: "-", pageName: "펀드비교팝업", pageType: "popup", pageID: "FD_01_01P", link: "", isAdminLinked: false, status: "", memo: [], },
+		{ createDate: "", modifyDate: "", category: "한화펀드", depth1: "펀드찾기", depth2: "-", depth3: "-", pageName: "펀드찾기", pageType: "page", pageID: "FD_01", link: "", isAdminLinked: true, status: "작업중", memo: [], },
+		{ createDate: "", modifyDate: "", category: "한화펀드", depth1: "펀드찾기", depth2: "펀드 상품 비교", depth3: "-", pageName: "펀드비교팝업", pageType: "popup", pageID: "FD_01_01P", link: "", isAdminLinked: false, status: "작업중", memo: [], },
 		{ createDate: "", modifyDate: "", category: "한화펀드", depth1: "펀드찾기", depth2: "펀드 상세 ", depth3: "-", pageName: "펀드상품상세", pageType: "page", pageID: "FD_01_02", link: "", isAdminLinked: true, status: "완료", memo: [], },
 		{ createDate: "", modifyDate: "", category: "한화펀드", depth1: "펀드찾기", depth2: "-", depth3: "일자별 기준가", pageName: "기준가팝업", pageType: "popup", pageID: "FD_01_02_01P", link: "", isAdminLinked: false, status: "완료", memo: [], },
 		{ createDate: "", modifyDate: "", category: "한화펀드", depth1: "펀드찾기", depth2: "-", depth3: "클래스비교", pageName: "클래스비교팝업", pageType: "popup", pageID: "FD_01_02_02P", link: "", isAdminLinked: false, status: "완료", memo: [], },
@@ -61,16 +62,16 @@ const iaPageAData = {
 		{ createDate: "", modifyDate: "", category: "회사소개", depth1: "오시는 길", depth2: "-", depth3: "-", pageName: "오시는길", pageType: "page", pageID: "CP_07", link: "", isAdminLinked: false, status: "", memo: [], },
 
 		// --- Footer ---
-		{ createDate: "", modifyDate: "", category: "Footer", depth1: "스튜어드십코드", depth2: "스튜어드십코드", depth3: "-", pageName: "스튜어드십코드", pageType: "tab", pageID: "FT_01_01T", link: "", isAdminLinked: true, status: "", memo: [], },
-		{ createDate: "", modifyDate: "", category: "Footer", depth1: "스튜어드십코드", depth2: "의결권행사내역", depth3: "-", pageName: "의결권행사내역", pageType: "tab", pageID: "FT_01_02T", link: "", isAdminLinked: true, status: "", memo: [], },
-		{ createDate: "", modifyDate: "", category: "Footer", depth1: "스튜어드십코드", depth2: "수탁자 책임 이행활동", depth3: "-", pageName: "수탁자책임이행활동", pageType: "tab", pageID: "FT_01_03T", link: "", isAdminLinked: true, status: "", memo: [], },
+		{ createDate: "", modifyDate: "", category: "Footer", depth1: "스튜어드십코드", depth2: "스튜어드십코드", depth3: "-", pageName: "스튜어드십코드", pageType: "tab", pageID: "FT_01_01T", link: "", isAdminLinked: true, status: "완료", memo: [], },
+		{ createDate: "", modifyDate: "", category: "Footer", depth1: "스튜어드십코드", depth2: "의결권행사내역", depth3: "-", pageName: "의결권행사내역", pageType: "tab", pageID: "FT_01_02T", link: "", isAdminLinked: true, status: "완료", memo: [], },
+		{ createDate: "", modifyDate: "", category: "Footer", depth1: "스튜어드십코드", depth2: "수탁자 책임 이행활동", depth3: "-", pageName: "수탁자책임이행활동", pageType: "tab", pageID: "FT_01_03T", link: "", isAdminLinked: true, status: "완료", memo: [], },
 		{ createDate: "", modifyDate: "", category: "Footer", depth1: "개인정보처리방침", depth2: "-", depth3: "-", pageName: "개인정보처리방침", pageType: "page", pageID: "FT_02", link: "", isAdminLinked: false, status: "", memo: [], },
 		{ createDate: "", modifyDate: "", category: "Footer", depth1: "신용정보활용체계", depth2: "-", depth3: "-", pageName: "신용정보활용체계", pageType: "page", pageID: "FT_03", link: "", isAdminLinked: false, status: "", memo: [], },
 		{ createDate: "", modifyDate: "", category: "Footer", depth1: "고객정보취급방침", depth2: "-", depth3: "-", pageName: "고객정보취급방침", pageType: "page", pageID: "FT_04", link: "", isAdminLinked: false, status: "", memo: [], },
 		{ createDate: "", modifyDate: "", category: "Footer", depth1: "윤리경영", depth2: "윤리경영", depth3: "-", pageName: "윤리경영", pageType: "tab", pageID: "FT_05_01T", link: "", isAdminLinked: false, status: "", memo: [], },
 		{ createDate: "", modifyDate: "", category: "Footer", depth1: "윤리경영", depth2: "윤리헌장", depth3: "-", pageName: "윤리헌장", pageType: "tab", pageID: "FT_05_02T", link: "", isAdminLinked: false, status: "", memo: [], },
 		{ createDate: "", modifyDate: "", category: "Footer", depth1: "윤리경영", depth2: "윤리행동지침", depth3: "-", pageName: "윤리행동지침", pageType: "tab", pageID: "FT_05_03T", link: "", isAdminLinked: false, status: "", memo: [], },
-		{ createDate: "", modifyDate: "", category: "Footer", depth1: "윤리경영", depth2: "사이버신문고", depth3: "-", pageName: "사이버신문고", pageType: "tab", pageID: "FT_05_04T", link: "", isAdminLinked: false, status: "", memo: [], },
+		{ createDate: "", modifyDate: "", category: "Footer", depth1: "윤리경영", depth2: "사이버신문고", depth3: "-", pageName: "사이버신문고", pageType: "tab", pageID: "FT_05_04T", link: "", isAdminLinked: false, status: "완료", memo: [], },
 		{ createDate: "", modifyDate: "", category: "Footer", depth1: "이메일무단수집거부", depth2: "-", depth3: "-", pageName: "이메일무단수집거부", pageType: "popup", pageID: "FT_06P", link: "", isAdminLinked: false, status: "", memo: [], },
 		{ createDate: "", modifyDate: "", category: "Footer", depth1: "법적고시", depth2: "-", depth3: "-", pageName: "법적고시", pageType: "popup", pageID: "FT_07P", link: "", isAdminLinked: false, status: "", memo: [], },
 		{ createDate: "", modifyDate: "", category: "Footer", depth1: "보호금융상품등록", depth2: "-", depth3: "-", pageName: "보호금융상품등록", pageType: "page", pageID: "FT_08", link: "", isAdminLinked: false, status: "", memo: [], },
