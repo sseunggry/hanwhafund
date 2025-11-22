103f966fbd27245dc8510cd12eaf603afeb93b40
sseung <dptmejf34@gmail.com>
2025-11-22 14:46:17 +0900

    전체메뉴 작업

--- src/guides/list/ia/data_a.js
+++ src/guides/list/ia/data_a.js
@@ -7,7 +7,7 @@ const iaPageAData = {
 		{ createDate: "2025-11-21", modifyDate: "", category: "메인", depth1: "검색", depth2: "", depth3: "-", pageName: "검색", pageType: "popup", pageID: "MA_03P", link: "", isAdminLinked: false, status: "완료", memo: [], },
 		{ createDate: "2025-11-21", modifyDate: "", category: "메인", depth1: "", depth2: "검색 결과", depth3: "-", pageName: "검색결과", pageType: "page", pageID: "MA_03_01", link: "", isAdminLinked: false, status: "완료", memo: [], },
 		{ createDate: "2025-11-21", modifyDate: "", category: "메인", depth1: "", depth2: "검색 결과", depth3: "-", pageName: "검색결과 없음", pageType: "page", pageID: "MA_03_02", link: "", isAdminLinked: false, status: "완료", memo: [], },
-		{ createDate: "", modifyDate: "", category: "메인", depth1: "전체 메뉴", depth2: "", depth3: "-", pageName: "전체메뉴", pageType: "popup", pageID: "MA_04P", link: "", isAdminLinked: false, status: "", memo: [], },
+		{ createDate: "", modifyDate: "", category: "메인", depth1: "전체 메뉴", depth2: "", depth3: "-", pageName: "전체메뉴", pageType: "popup", pageID: "MA_04P", link: "", isAdminLinked: false, status: "작업중", memo: [], },
 
 		// --- 한화펀드 ---
 		{ createDate: "2025-11-21", modifyDate: "", category: "한화펀드", depth1: "펀드찾기", depth2: "-", depth3: "-", pageName: "펀드찾기", pageType: "page", pageID: "FD_01", link: "", isAdminLinked: true, status: "완료", memo: [], },
@@ -59,7 +59,7 @@ const iaPageAData = {
 		{ createDate: "", modifyDate: "", category: "회사소개", depth1: "위험관리", depth2: "컴플라이언스 시스템", depth3: "-", pageName: "컴플라이언스시스템", pageType: "tab", pageID: "CP_05_03T", link: "", isAdminLinked: false, status: "", memo: [], },
 		{ createDate: "", modifyDate: "", category: "회사소개", depth1: "채용정보", depth2: "한화 인재상", depth3: "-", pageName: "한화인재상", pageType: "tab", pageID: "CP_06_01T", link: "", isAdminLinked: false, status: "", memo: [], },
 		{ createDate: "", modifyDate: "", category: "회사소개", depth1: "채용정보", depth2: "한화의 핵심가치", depth3: "-", pageName: "한화의책심가치", pageType: "tab", pageID: "CP_06_02T", link: "", isAdminLinked: false, status: "", memo: [], },
-		{ createDate: "", modifyDate: "", category: "회사소개", depth1: "오시는 길", depth2: "-", depth3: "-", pageName: "오시는길", pageType: "page", pageID: "CP_07", link: "", isAdminLinked: false, status: "", memo: [], },
+		{ createDate: "", modifyDate: "", category: "회사소개", depth1: "오시는 길", depth2: "-", depth3: "-", pageName: "오시는길", pageType: "page", pageID: "CP_07", link: "", isAdminLinked: false, status: "작업중", memo: [], },
 
 		// --- Footer ---
 		{ createDate: "2025-11-17", modifyDate: "", category: "Footer", depth1: "스튜어드십코드", depth2: "스튜어드십코드", depth3: "-", pageName: "스튜어드십코드", pageType: "tab", pageID: "FT_01_01T", link: "", isAdminLinked: true, status: "완료", memo: [], },
