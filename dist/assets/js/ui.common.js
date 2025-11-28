2f13b65f501cac3d77d89ecb25b9a1350bcc9789
seungyeonchoi <sychoi@pulip.com>
2025-11-28 16:03:51 +0900

    dist 회사소개

--- dist/assets/js/ui.common.js
+++ dist/assets/js/ui.common.js
@@ -449,17 +449,13 @@ const uiAccordion = {
       const $button = $item.find(".btn-accordion");
       const $content = $item.find(".accordion-collapse");
       
-			//임시
-			$item.addClass("active");
-      $content.show();
-			
-      // if ($item.hasClass("active")) {
-      //   $button.attr("aria-expanded", "true");
-      //   $content.show();
-      // } else {
-      //   $button.attr("aria-expanded", "false");
-      //   $content.hide();
-      // }
+      if ($item.hasClass("active")) {
+        $button.attr("aria-expanded", "true");
+        $content.show();
+      } else {
+        $button.attr("aria-expanded", "false");
+        $content.hide();
+      }
     });
 
     $(document).on("click", ".accordion .btn-accordion", function (e) {
