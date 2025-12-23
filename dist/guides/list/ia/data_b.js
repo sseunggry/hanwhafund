4e914125a9c543019d64b9c750c30d0fabb11076
seungyeonchoi <sychoi@pulip.com>
2025-12-23 11:30:38 +0900

    dist

--- dist/guides/list/ia/data_b.js
+++ dist/guides/list/ia/data_b.js
@@ -2,18 +2,18 @@ const iaPageBData = {
 	language: "en",
 	list: [
 		// --- 메인 ---
-		{ createDate: "2025-12-18",  modifyDate: "2025-12-23",  category: "Main", depth1: "",  depth2: "",  depth3: "-",  pageName: "메인",  pageType: "page",  pageID: "EN_MA_01",  link: "", isAdminLinked: true,  status: "완료",  memo: [{ detail: '2025-12-23: 글로벌네트워크 - 텍스트 수정' }] },
+		{ createDate: "2025-12-23",  modifyDate: "",  category: "Main", depth1: "",  depth2: "",  depth3: "-",  pageName: "메인",  pageType: "page",  pageID: "EN_MA_01",  link: "", isAdminLinked: true,  status: "완료",  memo: [{ detail: '2025-12-23: 글로벌네트워크 - 텍스트 수정' }] },
 
 		// --- Our Company ---
-		{ createDate: "", modifyDate: "", category: "Our Company", depth1: "Our Company ", depth2: "-", depth3: "-", pageName: "회사소개", pageType: "page", pageID: "EN_CP_01", link: "", isAdminLinked: true, status: "", memo: [], },
+		{ createDate: "2025-12-23", modifyDate: "", category: "Our Company", depth1: "Our Company ", depth2: "-", depth3: "-", pageName: "회사소개", pageType: "page", pageID: "EN_CP_01", link: "", isAdminLinked: true, status: "완료", memo: [], },
 		{ createDate: "2025-12-18", modifyDate: "", category: "Our Company", depth1: "Management Status", depth2: "Summary of Consolidated Statement of Comprehensive Income", depth3: "-", pageName: "요약 연결포괄 손익계산서", pageType: "tab", pageID: "EN_CP_02_01", link: "", isAdminLinked: false, status: "완료", memo: [], },
-		{ createDate: "2025-12-18", modifyDate: "", category: "Our Company", depth1: "-", depth2: "Summary of Consolidated Statement of Financial Position", depth3: "-", pageName: "요약 연결재무상태표", pageType: "tab", pageID: "EN_CP_02_02", link: "", isAdminLinked: false, status: "", memo: [], },
+		{ createDate: "2025-12-18", modifyDate: "", category: "Our Company", depth1: "-", depth2: "Summary of Consolidated Statement of Financial Position", depth3: "-", pageName: "요약 연결재무상태표", pageType: "tab", pageID: "EN_CP_02_02", link: "", isAdminLinked: false, status: "완료", memo: [], },
 		{ createDate: "2025-12-18", modifyDate: "", category: "Our Company", depth1: "The Hanwha Finance", depth2: "-", depth3: "-", pageName: "한화금융", pageType: "page", pageID: "EN_CP_03", link: "", isAdminLinked: true, status: "완료", memo: [{ detail: '영문 only' }], },
 
 		// --- Risk Management ---
 		{ createDate: "2025-12-18", modifyDate: "", category: "Risk Management", depth1: "Multi-monitoring system", depth2: "", depth3: "-", pageName: "운용모니터링 체계", pageType: "tab", pageID: "EN_RM_01", link: "", isAdminLinked: false, status: "완료", memo: [], },
-		{ createDate: "2025-12-18", modifyDate: "", category: "Risk Management", depth1: "Risk management system", depth2: "-", depth3: "-", pageName: "리스크 관리", pageType: "tab", pageID: "EN_RM_02", link: "", isAdminLinked: false, status: "", memo: [], },
-		{ createDate: "2025-12-18", modifyDate: "", category: "Risk Management", depth1: "Compliance system", depth2: "-", depth3: "-", pageName: "내부통제 점검체계", pageType: "tab", pageID: "EN_RM_03", link: "", isAdminLinked: false, status: "", memo: [], },
+		{ createDate: "2025-12-18", modifyDate: "", category: "Risk Management", depth1: "Risk management system", depth2: "-", depth3: "-", pageName: "리스크 관리", pageType: "tab", pageID: "EN_RM_02", link: "", isAdminLinked: false, status: "완료", memo: [], },
+		{ createDate: "2025-12-18", modifyDate: "", category: "Risk Management", depth1: "Compliance system", depth2: "-", depth3: "-", pageName: "내부통제 점검체계", pageType: "tab", pageID: "EN_RM_03", link: "", isAdminLinked: false, status: "완료", memo: [], },
 
 		// --- Global Network ---
 		{ createDate: "2025-12-18", modifyDate: "2025-12-23", category: "Global Network", depth1: "-", depth2: "-", depth3: "-", pageName: "글로벌네트워크", pageType: "page", pageID: "EN_GN_01", link: "", isAdminLinked: false, status: "완료", memo: [{ detail: '2025-12-23: 텍스트 수정' }], },
