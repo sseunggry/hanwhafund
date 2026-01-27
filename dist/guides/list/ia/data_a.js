fe3585aafbcb90e37183db148fd5ef8046cf8693
seungyeonchoi <sychoi@pulip.com>
2026-01-27 09:27:28 +0900

    dist

--- dist/guides/list/ia/data_a.js
+++ dist/guides/list/ia/data_a.js
@@ -7,7 +7,7 @@ const iaPageAData = {
 		{ createDate: "2025-11-21", modifyDate: "2026-01-05", category: "메인", depth1: "검색", depth2: "", depth3: "-", pageName: "검색", pageType: "popup", pageID: "MA_03P", link: "", isAdminLinked: false, status: "완료", memo: [{detail: `2026-01-05: 태그 띄어쓰기 추가`}], },
 		{ createDate: "2025-11-21", modifyDate: "2026-01-05", category: "메인", depth1: "", depth2: "검색 결과", depth3: "-", pageName: "검색결과", pageType: "page", pageID: "MA_03_01", link: "", isAdminLinked: false, status: "완료", memo: [{detail: `2026-01-05: 태그 띄어쓰기 추가`}, {detail: `2025-12-12: badge 컬러 light-orange 로 변경`}], },
 		{ createDate: "2025-11-21", modifyDate: "2026-01-05", category: "메인", depth1: "", depth2: "검색 결과", depth3: "-", pageName: "검색결과 없음", pageType: "page", pageID: "MA_03_02", link: "", isAdminLinked: false, status: "완료", memo: [{detail: `2026-01-05: 태그 띄어쓰기 추가`}, {detail: `2025-12-12: badge 컬러 light-orange 로 변경`}], },
-		{ createDate: "2025-11-25", modifyDate: "", category: "메인", depth1: "전체 메뉴", depth2: "", depth3: "-", pageName: "전체메뉴", pageType: "popup", pageID: "MA_04P", link: "", isAdminLinked: false, status: "완료", memo: [], },
+		{ createDate: "2025-11-25", modifyDate: "2026-01-27", category: "메인", depth1: "전체 메뉴", depth2: "", depth3: "-", pageName: "전체메뉴", pageType: "popup", pageID: "MA_04P", link: "", isAdminLinked: false, status: "완료", memo: [{detail: `2026-01-27: 한화체 변경(마크업수정)`}], },
 
 		// --- 한화펀드 ---
 		{ createDate: "2025-11-21", modifyDate: "2026-01-05", category: "한화펀드", depth1: "펀드찾기", depth2: "-", depth3: "-", pageName: "펀드찾기", pageType: "page", pageID: "FD_01", link: "", isAdminLinked: true, status: "완료", memo: [{detail: `2026-01-05: 태그 띄어쓰기 추가`}, {detail: `2025-12-22: 버튼명 '다시 선택' -> '설정 초기화' 수정`}, {detail: `2025-12-12: badge 컬러 light-orange 로 변경`}], },
@@ -46,7 +46,7 @@ const iaPageAData = {
 		{ createDate: "2025-11-10", modifyDate: "", category: "공시", depth1: "연금공시", depth2: "-", depth3: "-", pageName: "연금공시", pageType: "tab", pageID: "IF_05T", link: "", isAdminLinked: true, status: "완료", memo: [], },
 
 		// --- 회사소개 ---
-		{ createDate: "2025-12-23", modifyDate: "2026-01-19", category: "회사소개", depth1: "회사소개 ", depth2: "-", depth3: "-", pageName: "회사소개", pageType: "page", pageID: "CP_01", link: "", isAdminLinked: false, status: "완료", memo: [{detail: `2026-01-19: 버튼명 수정`}, {detail: `2026-01-16: 텍스트 수정`}, {detail: `2026-01-15: 마크업 수정`}, {detail: `2026-01-08: 마크업 수정`}, {detail: "2025-12-30: core value 부분 텍스트 수정"}], },
+		{ createDate: "2025-12-23", modifyDate: "2026-01-27", category: "회사소개", depth1: "회사소개 ", depth2: "-", depth3: "-", pageName: "회사소개", pageType: "page", pageID: "CP_01", link: "", isAdminLinked: false, status: "완료", memo: [{detail: `2026-01-27: 한화체 변경(마크업수정)`}, {detail: `2026-01-19: 버튼명 수정`}, {detail: `2026-01-16: 텍스트 수정`}, {detail: `2026-01-15: 마크업 수정`}, {detail: `2026-01-08: 마크업 수정`}, {detail: "2025-12-30: core value 부분 텍스트 수정"}], },
 		// { createDate: "", modifyDate: "", category: "회사소개", depth1: "주요 비즈니스", depth2: "PE", depth3: "-", pageName: "주요비즈니스PE", pageType: "tab", pageID: "CP_02_01T", link: "", isAdminLinked: true, status: "", memo: [], },
 		// { createDate: "", modifyDate: "", category: "회사소개", depth1: "주요 비즈니스", depth2: "VC", depth3: "-", pageName: "주요비즈니스VC", pageType: "tab", pageID: "CP_02_02T", link: "", isAdminLinked: true, status: "", memo: [], },
 		// { createDate: "", modifyDate: "", category: "회사소개", depth1: "주요 비즈니스", depth2: "FoFs", depth3: "-", pageName: "주요비즈니스FoFs", pageType: "tab", pageID: "CP_02_03T", link: "", isAdminLinked: true, status: "", memo: [], },
